82661e21039b60ba8fbf620242a64ab007dbf934
LukeSandersJet <lsanders@jet-airways.com>
2022-09-02 17:53:15 -0500

    add cors config

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
@@ -26,9 +27,13 @@ func main() {
 
 	coll := client.Database("playlist_db").Collection("playlist")
 	var result bson.M
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusAccepted, gin.H{
 			"msg": "wazzup",
 		})
 	})
+
+	r.Use(cors.Default())
+	r.Run()
 }
