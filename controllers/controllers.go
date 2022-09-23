60e5c1e1343ecc980b39e937a22b50ddb02d2595
LukeSandersJet <lsanders@jet-airways.com>
2022-09-23 17:57:20 -0500

    continue setting up controllers and services

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,10 +4,31 @@ import (
 	"net/http"
 
 	"github.com/MountainGator/playlist_CRUD/models"
+	"github.com/MountainGator/playlist_CRUD/services"
 	"github.com/gin-gonic/gin"
 )
 
-func CreateUser(c *gin.Context) {
+type UserController struct {
+	UserService services.UserService
+}
+
+type PlaylistController struct {
+	PlaylistService services.PlaylistService
+}
+
+func NewUserController(userservice services.UserService) UserController {
+	return UserController{
+		UserService: userservice,
+	}
+}
+
+func NewPlayController(playservice services.PlaylistService) PlaylistController {
+	return PlaylistController{
+		PlaylistService: playservice,
+	}
+}
+
+func (uc *UserController) CreateUser(c *gin.Context) {
 	var user models.User
 	if err := c.BindJSON(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -15,14 +36,32 @@ func CreateUser(c *gin.Context) {
 	}
 }
 
-func UserLogin(c *gin.Context) {
+func (uc *UserController) UserLogin(c *gin.Context) {
 
 }
 
-func NewPlaylist(c *gin.Context) {
+func (uc *UserController) UpdateUser(c *gin.Context) {
 
 }
 
-func FindPlaylist(c *gin.Context) {
-	
+func (uc *UserController) DeleteUser(c *gin.Context) {
+
+}
+
+func (pc *PlaylistController) NewPlaylist(c *gin.Context) {
+
+}
+
+func (pc *PlaylistController) FindPlaylist(c *gin.Context) {
+
+}
+
+func (pc *PlaylistController) AddSong(c *gin.Context) {
+
+}
+func (pc *PlaylistController) DeleteSong(c *gin.Context) {
+
+}
+func (pc *PlaylistController) DeletePlaylist(c *gin.Context) {
+
 }
--- services/user.service.go
+++ services/user.service.go
@@ -6,14 +6,15 @@ import (
 
 type UserService interface {
 	CreateUser(*models.User) error
-	Login(*string) (*models.User, error)
+	UserLogin(*string) (*models.User, error)
 	UpdateUser(*models.User) error
 	DeleteUser(*string) error
 }
 
 type PlaylistService interface {
 	NewPlaylist(*models.Playlist) error
-	AddSong(*models.Song) error
-	DeleteSong(*string) error
+	FindPlaylist(*string) error
+	AddSong(*string, *models.Song) error
+	DeleteSong(*string, *string) error
 	DeletePlaylist(*string) error
 }
