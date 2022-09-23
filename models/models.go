53bd0b68383860a2965b52fde671e6ffc9d7ace7
LukeSandersJet <lsanders@jet-airways.com>
2022-09-23 13:35:53 -0500

    make models file

--- models/models.go
+++ models/models.go
@@ -1,1 +1,20 @@
 package models
+
+type User struct {
+	Name     string `json:"name" bson:"name"`
+	Age      int    `json:"age" bson:"age"`
+	Username string `json:"username" bson:"username"`
+	Email    string `json:"email" bson:"email"`
+}
+
+type Song struct {
+	Artist string `json:"artist" bson:"artist"`
+	Title  string `json:"title" bson:"title"`
+	Album  string `json:"album" bson:"album"`
+}
+
+type Playlist struct {
+	Creator       string `json:"creator" bson:"creator"`
+	Playlist_name string `json:"playlist_name" bson:"playlist_name"`
+	Songs         []Song `json:"songs" bson:"songs"`
+}
