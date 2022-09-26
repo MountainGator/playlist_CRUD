e1d59b690ff98579200567a51689a9dd0907b5e4
LukeSandersJet <lsanders@jet-airways.com>
2022-09-26 18:03:32 -0500

    forgot to commit user service impl

--- services/user.impl.go
+++ services/user.impl.go
@@ -1,1 +1,58 @@
 package services
+
+import (
+	"context"
+	"errors"
+
+	"github.com/MountainGator/playlist_CRUD/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserServiceImpl struct {
+	usercollection *mongo.Collection
+	ctx            context.Context
+}
+
+func NewUserService(usercollection *mongo.Collection, ctx context.Context) UserService {
+	return &UserServiceImpl{
+		usercollection: usercollection,
+		ctx:            ctx,
+	}
+}
+
+func (u *UserServiceImpl) CreateUser(user *models.User) error {
+
+	_, err := u.usercollection.InsertOne(u.ctx, user)
+	return err
+}
+func (u *UserServiceImpl) UserLogin(name string) (*models.User, error) {
+	var user *models.User
+	query := bson.D{bson.E{Key: "name", Value: name}}
+	err := u.usercollection.FindOne(u.ctx, query).Decode(&user)
+	return user, err
+}
+func (u *UserServiceImpl) UpdateUser(user *models.User) error {
+	filter := bson.D{primitive.E{Key: "name", Value: user.Name}}
+	update := bson.D{
+		primitive.E{
+			Key: "$set",
+			Value: bson.D{
+				primitive.E{Key: "name", Value: user.Name},
+				primitive.E{Key: "age", Value: user.Age},
+				primitive.E{Key: "username", Value: user.Username},
+				primitive.E{Key: "email", Value: user.Email},
+			},
+		},
+	}
+	result, _ := u.usercollection.UpdateOne(u.ctx, filter, update)
+	if result.MatchedCount != 1 {
+		return errors.New("no matched document found for update")
+	}
+	return nil
+}
+func (u *UserServiceImpl) DeleteUser(user *models.User) error {
+	_, err := u.usercollection.InsertOne(u.ctx, user)
+	return err
+}
